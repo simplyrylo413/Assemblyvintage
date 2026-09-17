523d238d605e52bea6e7cbf730aa0bde5ddaacd7
simplyrylo413 <simplyrylo@gmail.com>
2026-09-17 12:48:26 -0400

    Load shared event alignment styles

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import { App } from "./App.jsx";
 import "./styles.css";
 import "./header-overrides.css";
+import "./event-alignment.css";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
