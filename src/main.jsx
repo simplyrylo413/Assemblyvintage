b1d6f5d646fb8337b154df682c0a11b94d286058
simplyrylo413 <simplyrylo@gmail.com>
2026-09-17 13:24:33 -0400

    Switch to rebuilt event system styles

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,7 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import { App } from "./App.jsx";
 import "./styles.css";
-import "./header-overrides.css";
-import "./event-alignment.css";
+import "./event-system.css";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
